35f39b03e5ab749b18c31223aca13946c25a2412
karim-sidhom <karimsidhom@proton.me>
2026-07-17 23:31:01 +0100

    new

--- channel.js
+++
@@ -1,6 +0,0 @@
-const channels = [
-  {"id": "BeinSports1.fr", "name": "beIN Sports 1", "country": "FR"},
-  {"id": "BeinSports2.fr", "name": "beIN Sports 2", "country": "FR"},
-  {"id": "BeinSports3.fr", "name": "beIN Sports 3", "country": "FR"},
-  {"id": "BeinSportsMax4.fr", "name": "beIN Sports Max 4", "country": "FR"}
-];
--- streams.js
+++
@@ -1,6 +0,0 @@
-const streams = [
-  {"channel": "BeinSports1.fr", "url": "https://ott.beinsports.com/manifest/bein1.m3u8"},
-  {"channel": "BeinSports2.fr", "url": "https://ott.beinsports.com/manifest/bein2.m3u8"},
-  {"channel": "BeinSports3.fr", "url": "https://ott.beinsports.com/manifest/bein3.m3u8"},
-  {"channel": "BeinSportsMax4.fr", "url": "https://ott.beinsports.com/manifest/beinmax4.m3u8"}
-];
